31d296551e56b425327c865de2a50832722910f8
Tuned-Automation <taylor@tunedautomation.com>
2025-08-09 10:45:55 +1200

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,7 +1,7 @@
 // Creates one global object you can use anywhere
 (function (global) {
   function greet(name) {
-    return `Kia ora, ${name}!`;
+    return `Kia ora, you big bad bitch! ${name}!`;
   }
   // everything lives under one tidy name to avoid conflicts
   global.TayScripts = { greet };
